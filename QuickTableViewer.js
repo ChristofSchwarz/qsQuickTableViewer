30482a6bc0f92ce8572208c00988397ccc93dd64
Christof Schwarz <christof.schwarz@gmx.at>
2018-10-29 08:45:42 +0100

    Update QuickTableViewer.js

--- QuickTableViewer.js
+++ QuickTableViewer.js
@@ -1,135 +1,60 @@
+/*
 var config = {
 	host: window.location.hostname,
 	prefix: "/",
 	port: window.location.port,
 	isSecure: window.location.protocol === "https:"
-};
+};*/
 define(["qlik", "jquery"],
 function (qlik, $) {
 
-	function newGUID(){
-		return( 
-			Math.random().toString(16).replace('.','').repeat(3).substr(1,8) 
-			+ '-' + Math.random().toString(16).replace('.','').repeat(2).substr(1,4)
-			+ '-' + Math.random().toString(16).replace('.','').repeat(2).substr(1,4)
-			+ '-' + Math.random().toString(16).replace('.','').repeat(2).substr(1,4)
-			+ '-' + Math.random().toString(16).replace('.','').repeat(4).substr(1,12)
-		);
-	};	
-
-
 	function convertToTable (layout){
-		var app = qlik.currApp(this); 
-		
-		var ownId = layout.qInfo.qId;
-		console.log('Selected table:' + layout.selectedTable, 'Field filter: ' +  layout.ignoreFieldPattern);
+
 		if (layout.selectedTable.length == 0) return; 
-		
+
+		var app = qlik.currApp(this);
+		var ownId = layout.qInfo.qId;		
 		var fieldList;
 		var thisObj;
+		var newTable;
+		console.log('Selected table:', layout.selectedTable);
+		console.log('Fields used:', layout.fieldPattern);
+		console.log('Fields filtered:', layout.ignoreFieldPattern);	
 
-		app.model.enigmaModel.evaluate('=Concat({<$Table={"' + layout.selectedTable + '"},'
-			+ '$Field={"(' + layout.fieldPattern + ')"}-{"(' + layout.ignoreFieldPattern + ')"}>} \'`\' & $Field & \'`\', \',\', $FieldNo)')
+		var setModifier = '{<$Table={"' + layout.selectedTable + '"},'
+			+ '$Field={"(' + layout.fieldPattern + ')"}-{"(' + layout.ignoreFieldPattern + ')"}>}';
+		// Use engine-formula to get a field list ... Concat($Field,',')
+		app.model.enigmaModel.evaluate("=Concat(" + setModifier + " '\"' & $Field & '\"', ',', $FieldNo)")
 		.then(function(ret) {
 			fieldList = eval('[' + ret + ']');
 			console.log('Field list: ', fieldList);
 			return(app.model.enigmaModel.getObject(ownId));
 		}).then(function(obj) {
 			thisObj = obj;
-			return(thisObj.getProperties());
-		}).then(function(prop) {
-			
-			// remove property entries of this extension 
-			delete prop.extensionMeta;
-			delete prop.version;
-			delete prop.selectedTable;
-			delete prop.ignoreFieldPattern;
-
-			// Manipulate the properties to become a table object
-			prop.qInfo.qType = "table";
-			prop.visualization = "table";
-
-			function getDimDef(newDim) {
-				return(
-				{
-					qLibraryId: "",
-					qDef: {
-						cId: newGUID(),
-						qGrouping: "N",
-						qFieldDefs: [newDim],	
-						qFieldLabels: [""],
-						qSortCriterias: [{
-							qSortByState: 0,
-							qSortByFrequency: 0,
-							qSortByNumeric: 1,
-							qSortByAscii: 1,
-							qSortByLoadOrder: 1,
-							qSortByExpression: 0,
-							qExpression: { qv: '' },
-							qSortByGreyness: 0
-						}],
-						qNumberPresentations: [],
-						autoSort: true,
-						qReverseSort: false,
-						qActiveField: 0,
-						qLabelExpression: "",
-						othersLabel: "Others",
-						textAlign: {auto: true, align: "left"},
-						representation: {type: "text", urlLabel: ""}
-					},
-					qNullSuppression: false,
-					qIncludeElemValue: false,
-					qShowTotal: false,
-					qShowAll: false,
-
-				});
-			};
-
-
-			prop.qHyperCubeDef = {
-				qStateName: "",
-				qDimensions: [],    
-				qMeasures: [],
-				qInterColumnSortOrder: [],
-				qColumnOrder: [],
-				columnOrder: [],
-				columnWidths: [],								
-				qSuppressZero: false,
-				qSuppressMissing: true,
-				qInitialDataFetch: [],
-				qReductionMode: "N",
-				qMode: "S",
-				qPseudoDimPos: -1,
-				qNoOfLeftDims: -1,
-				qAlwaysFullyExpanded: false,
-				qMaxStackedCells: 5000,
-				qPopulateMissing: false,
-				qShowTotalsAbove: false,
-				qIndentMode: false,
-				qSortbyYValue: 0,
-				qTitle: {"qv": ""},								
-				//"qCalcCond": {"qv": ""},
-				//"qCalcCondition": {"qCond": {"qv": ""},"qMsg": {"qv": ""}},
-			};					
-			prop.multiline = { "wrapTextInHeaders": false, "wrapTextInCells": false };
-			prop.title = layout.selectedTable;
-			prop.totals = {};
-
-			// iterate through array and add to the arrays of the qHyperCubeDef
-
-			fieldList.forEach(function(fieldName, i){
-				prop.qHyperCubeDef.qDimensions.push(getDimDef(fieldName));
-				prop.qHyperCubeDef.qInterColumnSortOrder.push(i);
-				prop.qHyperCubeDef.qColumnOrder.push(i);
-				prop.qHyperCubeDef.columnOrder.push(i);
-				prop.qHyperCubeDef.columnWidths.push(-1);
-			});
-
+			// Use visualization API to create a new table
+			return(app.visualization.create('table',fieldList, {title: layout.selectedTable}));
+		}).then(function(obj) {
+			newTable = obj;
+			// get properties of new table object
+			return(newTable.model.getProperties());
+		}).then(prop=>{
+			console.log('newTable properties',prop);
+			// manipulate the id to match the current extension object's id and
+			// overwrite current extension object with the new table properties
+			prop.qInfo.qId = ownId;
 			return(thisObj.setProperties(prop));
-
-		}).then(function(ret) {
+		}).then(ret=>{
+			newTable.close();
+			// change the object type to "table" also in the sheet properties 
+			var currSheet = qlik.navigation.getCurrentSheetId();
+			return(app.model.enigmaModel.getObject(currSheet.sheetId));
+		}).then(sheetObj=>{
+			sheetObj.properties.cells.forEach(cell=>{
+				if (cell.name == ownId) cell.type = 'table';
+			});
+			return(sheetObj.setProperties(sheetObj.properties));
+		}).then(ret=>{
 			console.log('Good bye. Object is now a table.');
-
 		}).catch(function(err) { 
 			console.error(err);	
 		});								
@@ -227,14 +152,13 @@ function (qlik, $) {
 		paint: function ($element, layout) {
 		
 			var helpUrl = 'https://github.com/ChristofSchwarz/qsQuickTableViewer/blob/master/README.md';
-			$element.html(`
-			<table height="100%" style="background-color:#eee;"><tr><td style="text-align:center;">
-			<p>This is a placeholder for your table.</p>
-			<p>After making your selections in the accordion menu on the right</p>
-			<p>this will become a standard Qlik Sense table (QlikTableViewer not needed then).</p>
-			<a class="lui-button" style="margin-top:5px;" href="${helpUrl}" target="_new">More info</a>
-			<td></tr></table>`);
-		
+			$element.html(
+			'<table height="100%" style="background-color:#eee;"><tr><td style="text-align:center;">'
+			+'<p>This is a placeholder for your table.</p>'
+			+'<p>After making your selections in the accordion menu on the right</p>'
+			+'<p>this will become a standard Qlik Sense table (QlikTableViewer not needed then).</p>'
+			+'<a class="lui-button" style="margin-top:5px;" href="' + helpUrl+ '" target="_new">More info</a>'
+			+'<td></tr></table>');
 			//needed for export
 			return qlik.Promise.resolve();
 		}
