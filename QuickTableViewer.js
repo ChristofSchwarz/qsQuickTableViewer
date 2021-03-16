d9afdb38968bb80c7f64fc82ebae80314b0af6ce
Christof Schwarz <christof.schwarz@databridge.ch>
2021-03-16 21:28:10 +0100

    Update QuickTableViewer.js

--- QuickTableViewer.js
+++ QuickTableViewer.js
@@ -1,24 +1,22 @@
-define(["qlik", 
-		"jquery",
-		"./properties"],
-function (qlik, $, properties) {
-	
+define(["qlik", "jquery", "./properties"
+], function (qlik, $, properties) {
+
 	return {
-		support : {
+		support: {
 			snapshot: false,
 			export: false,
-			exportData : false
+			exportData: false
 		},
 		definition: properties,
 		paint: function ($element, layout) {
 			var helpUrl = 'https://github.com/ChristofSchwarz/qsQuickTableViewer/blob/master/README.md';
 			$element.html(
-			'<table height="100%" style="background-color:#eee;"><tr><td style="text-align:center;">'
-			+'<p>This is a placeholder for your table.</p>'
-			+'<p>After making your selections in the accordion menu on the right</p>'
-			+'<p>this will become a standard Qlik Sense table (QlikTableViewer not needed then).</p>'
-			+'<a class="lui-button" style="margin-top:5px;" href="' + helpUrl+ '" target="_new">More info</a>'
-			+'<td></tr></table>');
+				'<table height="100%" style="background-color:#eee;"><tr><td style="text-align:center;">'
+				+ '<p>This is a placeholder for your table.</p>'
+				+ '<p>After making your selections in the accordion menu on the right</p>'
+				+ '<p>this will become a standard Qlik Sense table (QlikTableViewer not needed then).</p>'
+				+ '<a class="lui-button" style="margin-top:5px;" href="' + helpUrl + '" target="_new">More info</a>'
+				+ '<td></tr></table>');
 			//needed for export
 			return qlik.Promise.resolve();
 		}
